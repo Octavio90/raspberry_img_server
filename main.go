6dba0aab4d32c4fb145f62b71236ee9e1908e4db
Octavio <octavio@evenlift.io>
2019-06-17 17:13:07 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"os"
+	"log"
+	"fmt"
+	"flag"
+	"os/exec"
+	"net/http"
+	"html/template"
+)
+ 
+var port  = "8080"
+var imgs  = "imgs"
+var fname = fmt.Sprintf("./%v/test.jpg",imgs)
+var args  = []string{"-w","1640","-h","1232","-o",fname}
+
+func main(){
+	portFlag := flag.String("port", "", "Specify server port")
+	flag.Parse()
+
+	if *portFlag != ""  { port   = *portFlag }
+	fmt.Println(fmt.Sprintf("Serving on localhost:%v/view",port))
+
+	http.Handle(fmt.Sprintf("/%v/",imgs), http.StripPrefix(fmt.Sprintf("/%v/",imgs), http.FileServer(http.Dir(fmt.Sprintf("./%v",imgs)))))
+	http.HandleFunc("/view" , view )
+	http.HandleFunc("/capture", capture)
+	if err := http.ListenAndServe(":"+port, nil); err != nil { log.Panic(err) }
+}
+
+func view(w http.ResponseWriter, r *http.Request) {
+	t,err := template.ParseFiles("index.html")
+	if err != nil { log.Panic(err) }
+	t.Execute(w,"Welcome")
+}
+
+func capture(w http.ResponseWriter, r *http.Request){
+	if _, err := os.Stat(fname); err == nil { 
+		//fmt.Println("Removing file",fname) 
+		os.Remove(fname)
+	} 
+	//fmt.Println("Taking picture on ",fname)
+	cmd     := exec.Command("raspistill", args...)
+	if err  := cmd.Start(); err != nil {log.Panic(err)}
+	
+	err     := cmd.Wait()
+	if err  != nil { log.Fatal(err) }
+	http.Redirect(w,r,"/view",http.StatusSeeOther)
+}
+
